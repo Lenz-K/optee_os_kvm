4c0adb8e7115003d87ffbe3b343a943b1ea4edc6
Lenz <lenz.karbaumer@hm.edu>
2021-10-19 22:50:14 +0200

    Remove unnecessary target

--- kvm.mk
+++ kvm.mk
@@ -35,7 +35,6 @@ GICV3 ?= n
 ################################################################################
 BINARIES_PATH		?= $(ROOT)/out/bin
 MODULE_OUTPUT		?= $(ROOT)/out/kernel_modules
-MKIMAGE_PATH		?= $(UBOOT_PATH)/tools
 
 ROOTFS_GZ		?= $(BINARIES_PATH)/rootfs.cpio.gz
 ROOTFS_UGZ		?= $(BINARIES_PATH)/rootfs.cpio.uboot
@@ -44,27 +43,11 @@ KERNEL_IMAGE		?= $(LINUX_PATH)/arch/arm64/boot/Image
 KERNEL_IMAGEGZ		?= $(LINUX_PATH)/arch/arm64/boot/Image.gz
 KERNEL_UIMAGE		?= $(BINARIES_PATH)/uImage
 
-# Load and entry addresses
-KERNEL_ENTRY		?= 0x40400000
-KERNEL_LOADADDR		?= 0x40400000
-ROOTFS_ENTRY		?= 0x44000000
-ROOTFS_LOADADDR		?= 0x44000000
-
-ifeq ($(UBOOT),y)
-BL33_BIN		?= $(UBOOT_BIN)
-BL33_DEPS		?= u-boot
-else
-BL33_BIN		?= $(EDK2_BIN)
-BL33_DEPS		?= edk2
-endif
-
 ################################################################################
 # Targets
 ################################################################################
 TARGET_DEPS := buildroot linux optee-os
 TARGET_CLEAN := buildroot-clean linux-clean optee-os-clean check-clean
-
-TARGET_DEPS 		+= $(BL33_DEPS)
 
 all: $(TARGET_DEPS)
 
@@ -113,33 +96,4 @@ OPTEE_OS_COMMON_FLAGS += DEBUG=$(DEBUG) CFG_ARM_GICV3=$(GICV3)
 optee-os: optee-os-common
 
 optee-os-clean: optee-os-clean-common
-
-################################################################################
-# mkimage - create images to be loaded by U-Boot
-################################################################################
-# Without the objcopy, the uImage will be 10x bigger.
-$(KERNEL_UIMAGE): u-boot linux | $(BINARIES_PATH)
-	${AARCH64_CROSS_COMPILE}objcopy -O binary \
-					-R .note \
-					-R .comment \
-					-S $(LINUX_PATH)/vmlinux \
-					$(BINARIES_PATH)/linux.bin
-	$(MKIMAGE_PATH)/mkimage -A arm64 \
-				-O linux \
-				-T kernel \
-				-C none \
-				-a $(KERNEL_LOADADDR) \
-				-e $(KERNEL_ENTRY) \
-				-n "Linux kernel" \
-				-d $(BINARIES_PATH)/linux.bin $(KERNEL_UIMAGE)
-
-$(ROOTFS_UGZ): u-boot buildroot | $(BINARIES_PATH)
-	ln -sf $(ROOT)/out-br/images/rootfs.cpio.gz $(BINARIES_PATH)
-	$(MKIMAGE_PATH)/mkimage -A arm64 \
-				-T ramdisk \
-				-C gzip \
-				-a $(ROOTFS_LOADADDR) \
-				-e $(ROOTFS_ENTRY) \
-				-n "Root file system" \
-				-d $(ROOTFS_GZ) $(ROOTFS_UGZ)
 
