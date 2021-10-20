e7d2f12364d301827b4bd17e941546384382c2cc
Lenz <lenz.karbaumer@hm.edu>
2021-10-20 16:27:46 +0200

    Improve Documentation

--- build_kvm.sh
+++ build_kvm.sh
@@ -6,7 +6,9 @@ echo Building with $JOBS jobs
 echo Initializing Repositories:
 # Use a custom manifest file
 repo init -u https://github.com/Lenz-K/optee_os_kvm.git -b plat-kvm -m kvm.xml
+# Run this to update the repositories
 repo sync -j $JOBS
+
 cd build
 # Use a custom Makefile
 ln -s ../optee_os/kvm.mk Makefile
